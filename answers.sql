b6d9f038e5b1dce7702cac35b0e2638155db5230
shunsui254 <44553732+shunsui254@users.noreply.github.com>
2025-05-07 20:28:32 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -13,6 +13,11 @@ JOIN (
     SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4
 ) numbers ON CHAR_LENGTH(Products) - CHAR_LENGTH(REPLACE(Products, ',', '')) >= numbers.n - 1;
 
+
+
+
+
+
 -- Question 2
 
 -- Transform OrderDetails table into 2NF
